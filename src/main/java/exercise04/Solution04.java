94833291cdab148056d0fba7072c066db041dd0a
richard lopez <richardlopez813>
2021-09-05 22:08:52 -0400

    final

--- src/main/java/exercise04/Solution04.java
+++ src/main/java/exercise04/Solution04.java
@@ -1,5 +1,18 @@
 package exercise04;
 
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solution
+ *  Copyright 2021 Richard Lopez
+ */
+
+/*
+    1. Input Scanner
+    2. prompt user for noun
+    3. Scan for noun
+    4. Repeat process for verb, adjective, adverb
+    5. Output madlibs game with all the inputs
+ */
+
 import java.util.Scanner;
 
 public class Solution04 {
